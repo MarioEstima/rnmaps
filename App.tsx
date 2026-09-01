3d4688c90ddf1f533477b2d7b299fa6fff9052ed
marioestima <marioestimahackz@gmail.com>
2026-09-01 02:42:55 +0100

    Add MapPin Camera Animation

--- App.tsx
+++ App.tsx
@@ -1,20 +1,67 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useEffect, useState, useRef } from "react";
+import MapView, { Marker } from "react-native-maps";
+import { Text, View } from "react-native";
+import { styles } from "./styles";
+import {
+  requestForegroundPermissionsAsync,
+  getCurrentPositionAsync,
+  type LocationObject,
+  watchPositionAsync,
+  LocationAccuracy,
+} from "expo-location";
 
 export default function App() {
+  const [location, setLocation] = useState<LocationObject | null>(null);
+  const mapRef = useRef<MapView | null>(null);
+  async function requestLocationPermission() {
+    const { granted } = await requestForegroundPermissionsAsync();
+    if (granted) {
+      const currentPosition = await getCurrentPositionAsync();
+      setLocation(currentPosition);
+    }
+
+    useEffect(() => {
+      requestLocationPermission();
+    }, []);
+
+    useEffect(() => {
+      watchPositionAsync(
+        {
+          accuracy: LocationAccuracy.High,
+          timeInterval: 1000,
+          distanceInterval: 1,
+        },
+        (response) => {
+          setLocation(response);
+          mapRef.current?.animateCamera({
+            pitch: 70,
+            center: response.coords,
+          });
+        },
+      );
+    }, []);
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {location && (
+        <MapView
+          ref={mapRef}
+          style={styles.map}
+          initialRegion={{
+            latitude: location.coords.latitude,
+            longitude: location.coords.longitude,
+            latitudeDelta: 0.005,
+            longitudeDelta: 0.005,
+          }}
+        >
+          <Marker
+            coordinate={{
+              latitude: location.coords.latitude,
+              longitude: location.coords.longitude,
+            }}
+          />
+        </MapView>
+      )}
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
